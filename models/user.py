cbda90e9dfd2fa935e1bd28250c857ae55c86b46
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2024-09-10 21:04:53 +1000

    Adding CLI Controllers

--- models/user.py
+++ models/user.py
@@ -8,11 +8,11 @@ class User(db.Model):
     # Attributes of the table
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String,)
-    email = db.Columns(db.String, nullable=False, unique=True)
-    password = db.Columns(db.String, nullable=False)
-    is_admin = db.Columns(db.Boolean, default=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
     
-class UserSchema(ma.Scheme):
+class UserSchema(ma.Schema):
     class Meta:
         fields=("id", "name", "email", "password", "is_admin")
         
